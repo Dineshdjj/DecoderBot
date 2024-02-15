ef69fbae1502290253084d0a3b12599596f54135
DINESH.R <dd125684@gmail.com>
2024-02-15 23:09:13 +0530

    responsiveness

--- amazon-clone-page/home.js
+++ amazon-clone-page/home.js
@@ -1,134 +1,86 @@
 import { todayDeal } from "./todayDeal.js";
 
-let slideBtnLeft = document.getElementById("slide-btn-left");
-let slideBtnRight = document.getElementById("slide-btn-right");
-let imgItem = document.querySelectorAll(".image-item");
-
-console.log(imgItem.length - 1);
+const slideBtnLeft = document.getElementById("slide-btn-left");
+const slideBtnRight = document.getElementById("slide-btn-right");
+const imgItem = document.querySelectorAll(".image-item");
+const todayDealProductListEl = document.querySelector(".today_deals_product_list");
+const today_deal_btn_prevEl = document.getElementById("today_deal_btn_prev");
+const today_deal_btn_nextEl = document.getElementById("today_deal_btn_next");
+const today_deals_product_itemEl = document.querySelectorAll(".today_deals_product_item");
+const sidebarNavigationEl = document.getElementById("sidebar-container-navigation-id");
+const sidebarOpenNavigationEl = document.getElementById("open-nav-sidebar");
+const sidebarCloseNavigationEl = document.getElementById("sidebar-navigation-close");
 
 let startSlider = 0;
-let endSlider = (imgItem.length - 1) * 100;  // 700
-
-slideBtnLeft.addEventListener("click", handleLeftBtn);
-
-function handleLeftBtn() {
-    if (startSlider <= 0) {
-        startSlider += 100;
-    } else {
-        startSlider = -(imgItem.length - 1) * 100;
-    }
-    imgItem.forEach(element => {
-        element.style.transform = `translateX(${startSlider}%)`;
-    });
-}
-
-slideBtnRight.addEventListener("click", handleRightBtn);
-
-function handleRightBtn() {
-    if (startSlider > -(imgItem.length - 1) * 100) {
-        startSlider -= 100;
-    } else {
-        startSlider = 0;
-    }
-    imgItem.forEach(element => {
-        element.style.transform = `translateX(${startSlider}%)`;
-    });
-}
-
-//render automatic
-function renderSlideAuto() {
-    if (startSlider >= -endSlider + 100) {
-        handleRightBtn();
-    } else {
-        startSlider = 0;
-    }
-}
-setInterval(renderSlideAuto, 2000);
-
-
-
-/***** sidebar navigation  */
-const sidebarNavigationEl = document.getElementById("sidebar-container-navigation-id")
-const sidebarOpenNavigationEl = document.getElementById("open-nav-sidebar")
-const sidebarCloseNavigationEl = document.getElementById("sidebar-navigation-close")
-
-
-//  console.log(sidebarNavigationEl)
-
-sidebarOpenNavigationEl.addEventListener("click", () => {
-    sidebarNavigationEl.classList.toggle("slidebar-show")
-})
-sidebarCloseNavigationEl.addEventListener("click", () => {
-    sidebarNavigationEl.classList.toggle("slidebar-show")
-})
-
-
-
-
-//today deals 
-console.log(todayDeal)
-let todayDealProductListEl = document.querySelector(".today_deals_product_list")
-console.log(todayDealProductListEl)
-
-let todayDealProductHTML = ""
-
-let todayDeallength = todayDeal.length
-
-for (let i = 0; i < todayDeallength; i++) {
-    // console.log(todayDeal[i])
-
-    todayDealProductHTML += `
-        <div class="today_deals_product_item">
-                <div class="todayDeals_product_image">
-                    <img src=${todayDeal[i].img} />
-                </div>
-            
-
-
-            <div class="discount_Contaienr">
-                <a href="#">Up to ${todayDeal[i].discount}% off</a>
-                <a href="#">${todayDeal[i].DealOfDay}</a>
-            </div>
-            <p>${todayDeal[i].desc}</p>
-        </div>
-    `
-}
-
-todayDealProductListEl.innerHTML = todayDealProductHTML
-//  console.log(todayDealProductHTML)
-
-let today_deal_btn_prevEl = document.getElementById("today_deal_btn_prev")
-let today_deal_btn_nextEl = document.getElementById("today_deal_btn_next")
-let today_deals_product_itemEl = document.querySelectorAll(".today_deals_product_item")
-
+let endSlider = (imgItem.length - 1) * 100;
 let startProduct = 0;
 
+slideBtnLeft.addEventListener("click", handleLeftBtn);
+slideBtnRight.addEventListener("click", handleRightBtn);
+sidebarOpenNavigationEl.addEventListener("click", toggleSidebarNavigation);
+sidebarCloseNavigationEl.addEventListener("click", toggleSidebarNavigation);
+today_deal_btn_prevEl.addEventListener("click", handlePrevProduct);
+today_deal_btn_nextEl.addEventListener("click", handleNextProduct);
 
-today_deal_btn_prevEl.addEventListener("click", () => {
+renderTodayDealProducts();
+setInterval(renderSlideAuto, 2000);
 
-   
-    if(startProduct < 0){
-        startProduct += 500
-    }
-    if(startProduct > -2000){
-        today_deals_product_itemEl.forEach(el =>{
-            el.style.transform = `translateX(${startProduct}%)`
-        })
-    }
+function handleLeftBtn() {
+    startSlider = (startSlider <= 0) ? startSlider + 100 : -(imgItem.length - 1) * 100;
+    moveSlider();
+}
 
-})
+function handleRightBtn() {
+    startSlider = (startSlider > -(imgItem.length - 1) * 100) ? startSlider - 100 : 0;
+    moveSlider();
+}
 
-today_deal_btn_nextEl.addEventListener("click", () => {
-    // alert("next")
-    
-    if(startProduct > -1500){
-        startProduct -= 500
-    }
+function moveSlider() {
+    imgItem.forEach(element => {
+        element.style.transform = `translateX(${startSlider}%)`;
+    });
+}
 
-    today_deals_product_itemEl.forEach(el =>{
-        el.style.transform = `translateX(${startProduct}%)`
-    })
-    
-    
-})
+function renderSlideAuto() {
+    startSlider = (startSlider >= -endSlider + 100) ? startSlider - 100 : 0;
+    moveSlider();
+}
 
+function toggleSidebarNavigation() {
+    sidebarNavigationEl.classList.toggle("slidebar-show");
+}
+
+function renderTodayDealProducts() {
+    let todayDealProductHTML = "";
+    todayDeal.forEach(deal => {
+        todayDealProductHTML += `
+            <div class="today_deals_product_item">
+                <div class="todayDeals_product_image">
+                    <img src="${deal.img}" />
+                </div>
+                <div class="discount_Contaienr">
+                    <a href="#">Up to ${deal.discount}% off</a>
+                    <a href="#">${deal.DealOfDay}</a>
+                </div>
+                <p>${deal.desc}</p>
+            </div>
+        `;
+    });
+    todayDealProductListEl.innerHTML = todayDealProductHTML;
+}
+
+function handlePrevProduct() {
+    startProduct = (startProduct < 0) ? startProduct + 500 : startProduct;
+    moveProductSlider();
+}
+
+function handleNextProduct() {
+    startProduct = (startProduct > -1500) ? startProduct - 500 : startProduct;
+    moveProductSlider();
+}
+
+function moveProductSlider() {
+    today_deals_product_itemEl.forEach(item => {
+        item.style.transform = `translateX(${startProduct}%)`;
+    });
+}
