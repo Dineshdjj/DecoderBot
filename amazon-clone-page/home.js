fdd5ccf783aaf770098c24dfee0b37f3c63e5150
DINESH.R <dd125684@gmail.com>
2024-02-15 22:22:53 +0530

    modification in image slider

--- amazon-clone-page/home.js
+++ amazon-clone-page/home.js
@@ -1,49 +1,49 @@
-import { todayDeal } from "./todayDeal.js"
+import { todayDeal } from "./todayDeal.js";
 
-let slideBtnLeft = document.getElementById("slide-btn-left")
-let slideBtnRight = document.getElementById("slide-btn-right")
-let imgItem = document.querySelectorAll(".image-item")
+let slideBtnLeft = document.getElementById("slide-btn-left");
+let slideBtnRight = document.getElementById("slide-btn-right");
+let imgItem = document.querySelectorAll(".image-item");
 
+console.log(imgItem.length - 1);
 
-console.log(imgItem.length - 1)
+let startSlider = 0;
+let endSlider = (imgItem.length - 1) * 100;  // 700
 
-let startSlider = 0
-let endSlider = (imgItem.length - 1) * 100  // 700
-
-slideBtnLeft.addEventListener("click", handleLeftBtn)
+slideBtnLeft.addEventListener("click", handleLeftBtn);
 
 function handleLeftBtn() {
-    if (startSlider < 0) {
-        startSlider = startSlider + 100;
+    if (startSlider <= 0) {
+        startSlider += 100;
+    } else {
+        startSlider = -(imgItem.length - 1) * 100;
     }
     imgItem.forEach(element => {
         element.style.transform = `translateX(${startSlider}%)`;
-    })
+    });
 }
 
-slideBtnRight.addEventListener("click", handleRightBtn)
+slideBtnRight.addEventListener("click", handleRightBtn);
 
 function handleRightBtn() {
-    if (startSlider >= -endSlider + 100) {
-        startSlider = startSlider - 100;
+    if (startSlider > -(imgItem.length - 1) * 100) {
+        startSlider -= 100;
+    } else {
+        startSlider = 0;
     }
     imgItem.forEach(element => {
         element.style.transform = `translateX(${startSlider}%)`;
-    })
-
+    });
 }
+
 //render automatic
 function renderSlideAuto() {
-
     if (startSlider >= -endSlider + 100) {
-        handleRightBtn()
-    }
-    else {
+        handleRightBtn();
+    } else {
         startSlider = 0;
     }
 }
 setInterval(renderSlideAuto, 2000);
-
 
 
 
