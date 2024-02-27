2ba09be15c933f4278dd7deba695bba40af30143
DINESH.R <94304959+Dineshdjj@users.noreply.github.com>
2024-02-27 15:13:35 +0000

    ..

--- weather-site/weather.js
+++ weather-site/weather.js
@@ -37,19 +37,19 @@ async function checkWeather(city){
 
         switch(weather_data.weather[0].main){
             case 'Clouds':
-                weather_img.src = "weather-site/assets/cloud.png";
+                weather_img.src = "/weather-site/assets/cloud.png";
                 break;
             case 'Clear':
-                weather_img.src = "weather-site/assets/clear.png";
+                weather_img.src = "/weather-site/assets/clear.png";
                 break;
             case 'Rain':
-                weather_img.src = "weather-site/assets/rain.png";
+                weather_img.src = "/weather-site/assets/rain.png";
                 break;
             case 'Mist':
-                weather_img.src = "weather-site/assets/mist.png";
+                weather_img.src = "/weather-site/assets/mist.png";
                 break;
             case 'Snow':
-                weather_img.src = "weather-site/assets/snow.png";
+                weather_img.src = "/weather-site/assets/snow.png";
                 break;
         }
 
