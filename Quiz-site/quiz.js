af5627e3da6a0bfa45ba88bea171fcbd0fdb0c1f
dineshdjj <dd125684@gmail.com>
2024-02-28 19:12:05 +0530

    questions

---
+++ Quiz-site/quiz.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+const questionBox = document.querySelector('.question');
+const choicesBox = document.querySelector('.choices');
+const nextBtn = document.querySelector('.nextBtn');
+const scoreCard = document.querySelector('.scoreCard');
+const alert = document.querySelector('.alert');
+const startBtn = document.querySelector('.startBtn');
+const timer = document.querySelector('.timer');
+
+
+// Make an array of objects that stores question, choices of question and answer
+const quiz = [
+    {
+        question: "Q. Which of the following is not a CSS box model property?",
+        choices: ["margin", "padding", "border-radius", "border-collapse"],
+        answer: "border-collapse"
+    },
+    {
+        question: "Q. Which of the following is not a valid way to declare a function in JavaScript?",
+        choices: ["function myFunction() {}", " let myFunction = function() {};", "myFunction: function() {}", "const myFunction = () => {};"],
+        answer: "myFunction: function() {}"
+    },
+    {
+        question: "Q. Which of the following is not a JavaScript data type?",
+        choices: ["string", "boolean", "object", "float"],
+        answer: "float"
+    },
+    {
+        question: "Q. What is the purpose of the this keyword in JavaScript?",
+        choices: ["It refers to the current function.", "It refers to the current object.", "It refers to the parent object.", " It is used for comments."],
+        answer: "It refers to the current object."
+    }
+];
